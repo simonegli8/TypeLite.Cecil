6a5bfa1e6b8261c9221f71433c0defd0ec1b5ba9
Niklas B?ckstr?m <nikal@nikal.se>
2016-08-10 18:37:40 +0200

    #63 #105 Allow ignored self-referencing enumerables
    This should allow JSON.NET JToken (and others) type of properties without breaking the TypeLite build, but only if those properties are ignored.

---
+++ TypeLite.Tests/RegressionTests/Issue63_SelfReferencingEnumerable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TypeLite.Tests.RegressionTests {
+    public class Issue63_SelfReferencingEnumerable {
+        /// <summary>
+        /// When a self-referencing enumerable is present but ignored, it shouldn't break the build.
+        /// </summary>
+        [Fact]
+        public void WhenBuild_NoSelfReferencingEnumerableInfiniteLoop() {
+            var target = new TsModelBuilder();
+            target.Add(typeof(IgnoredSelfReferencingEnumerableWrapper));
+
+            // May cause infinite loop or stack overflow, if not handled correctly.
+            target.Build();
+        }
+    }
+
+    public class IgnoredSelfReferencingEnumerableWrapper {
+        [TsIgnore]
+        public SelfReferencingEnumerable MyProperty { get; set; }
+    }
+
+    public class SelfReferencingEnumerable : IEnumerable<SelfReferencingEnumerable> {
+        public IEnumerator<SelfReferencingEnumerable> GetEnumerator() {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            throw new NotImplementedException();
+        }
+    }
+}
