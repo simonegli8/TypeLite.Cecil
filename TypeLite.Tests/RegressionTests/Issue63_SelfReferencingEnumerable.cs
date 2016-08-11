6406c7cda7fa68b4b39cd662edebf4cc84774862
Niklas B?ckstr?m <nikal@nikal.se>
2016-08-11 15:29:08 +0200

    #63 #105 Fix self-referencing enumerables (emit TsType.Any)
    Tip: Use a custom TsMemberTypeFormatter to make Newtonsoft.Json.Linq.JObject emit as "any" instead of "any[]" (see the added unit test for an example).

--- TypeLite.Tests/RegressionTests/Issue63_SelfReferencingEnumerable.cs
+++ TypeLite.Tests/RegressionTests/Issue63_SelfReferencingEnumerable.cs
@@ -19,11 +19,59 @@ namespace TypeLite.Tests.RegressionTests {
             // May cause infinite loop or stack overflow, if not handled correctly.
             target.Build();
         }
+
+        /// <summary>
+        /// A self-referencing enumerable should emit type "any".
+        /// </summary>
+        [Fact]
+        public void SelfReferencingEnumerableGenerateAny() {
+            string output = TypeScript.Definitions()
+                .For<SelfReferencingEnumerableWrapper>()
+                .Generate(TsGeneratorOutput.Properties);
+
+            Assert.Contains("MyProperty: any;", output);
+        }
+        
+        [Fact]
+        public void SelfReferencingEnumerableInheritorGenerateAnyArray() {
+            var ts = TypeScript.Definitions();
+            ts
+                .WithMemberTypeFormatter((tsProperty, memberTypeName) => {
+                    // The following is a workaround proof-of-concept for the JSON.NET JObject class (among others).
+                    // Without this, it would be similar to a List<JToken> (in the way that they both
+                    // implement IEnumerable<JToken>) and emit type "any[]" instead of the desired type "any".
+                    // Example: tsProperty.PropertyType.Type.FullName == "Newtonsoft.Json.Linq.JObject"
+                    if (tsProperty.PropertyType.Type.Name == "SelfReferencingEnumerableInheritor") {
+                        // No "[]" emitted.
+                        return memberTypeName;
+                    }
+                    return ts.ScriptGenerator.DefaultMemberTypeFormatter(tsProperty, memberTypeName);
+                })
+                .For<SelfReferencingEnumerableInheritorWrapper>();
+            string output = ts
+                .Generate(TsGeneratorOutput.Properties);
+            
+            Assert.Contains("MyInheritorProperty: any;", output);
+            Assert.Contains("MyArrayProperty: any[];", output);
+            Assert.Contains("MyListProperty: any[];", output);
+        }
     }
 
     public class IgnoredSelfReferencingEnumerableWrapper {
         [TsIgnore]
+        public SelfReferencingEnumerable MyIgnoredProperty { get; set; }
+    }
+
+    public class SelfReferencingEnumerableWrapper {
         public SelfReferencingEnumerable MyProperty { get; set; }
+    }
+
+    public class SelfReferencingEnumerableInheritorWrapper {
+        public SelfReferencingEnumerableInheritor MyInheritorProperty { get; set; }
+
+        public SelfReferencingEnumerable[] MyArrayProperty { get; set; }
+
+        public List<SelfReferencingEnumerable> MyListProperty { get; set; }
     }
 
     public class SelfReferencingEnumerable : IEnumerable<SelfReferencingEnumerable> {
@@ -34,5 +82,8 @@ namespace TypeLite.Tests.RegressionTests {
         IEnumerator IEnumerable.GetEnumerator() {
             throw new NotImplementedException();
         }
+    }
+
+    public class SelfReferencingEnumerableInheritor : SelfReferencingEnumerable {
     }
 }
